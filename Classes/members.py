aa250359a85e529de2226a0b257f575f1e35f8dc
Tejaswi <tejaswikanneganti20@gmail.com>
2025-02-17 23:44:28 -0600

    Update members.py

--- Classes/members.py
+++ Classes/members.py
@@ -1,113 +1,110 @@
-
-
 class Member:
 
     member_serial_number = 0
 
-    def __init__(self, email=None, fname=None, lname=None,  phone_number = None, dob = None, age=None, address=None, salary=None,):
+    def __init__(self, email, fname=None, lname=None,  age=None, address=None, salary=None):
         self.id = None
         self.first_name = fname
         self.last_name = lname
         self.email = email
         self.phone_number = None
+        self.date_of_birth = None
         self.age = age
         self.address = address
         self.salary = salary
-        self.date_of_birth = dob
         self.__ssn = None
-
-    @staticmethod
-    def retrieve_serial_number_static():
-        return Member.member_serial_number
+        self.db_connection()
     
-    @classmethod
-    def retrieve_serial_number_class(cls):
-        #return cls.member_serial_number both are valid
-        return Member.member_serial_number
-    
-    #private method
-    def __retrieve_ssn(self):
-        print(self.__ssn)
-
-    def retrieve_member_details(self):
-        member_serial_number = 20
-        #Retrieve details from database using Email
-        return "details"
-    
-    def create_new_member(self):
-        import configparser
+    def db_connection(self):
         import os
         from dotenv import load_dotenv
         import psycopg2
-        from manage_tables import create_table
-        #from members import create_member, retrieve_members, update_member, delete_member
-        #from transactions import create_transaction, retrieve_transactions
 
+        # Load environment variables from .env file
+        load_dotenv()
 
-        # Initialize the config parser
-        config = configparser.ConfigParser()
-        config.read('credentials.cfg')
-
-        # Database connection details
-        # Default PostgreSQL port
-        # Establishing the connection
+        # Database connection setup
         try:
-            connection = psycopg2.connect(
-                dbname=config['postgres']['DB_NAME'],
-                user=config['postgres']['DB_USER'],
-                password=config['postgres']['DB_PASSWORD'],
-                host=config['postgres']['DB_HOST'],
-                port=config['postgres']['DB_PORT'],
+            self.connection = psycopg2.connect(
+                dbname=os.getenv('DB_NAME'),
+                user=os.getenv('DB_USER'),
+                password=os.getenv('DB_PASSWORD'),
+                host=os.getenv('DB_HOST'),
+                port=os.getenv('DB_PORT'),
                 sslmode="require"
             )
             print("Database connection successful!")
         except Exception as e:
-            print("Error connecting to the database:", e)
+            print("Error connecting to the database",e)
 
-        #2.3 Creating a Cursor Object and Executing Queries
-        #Once connected, a cursor object is used to execute SQL queries:
-
-        cursor = connection.cursor()
-        cursor.execute("SELECT version();")
-        print("PostgreSQL version:", cursor.fetchone())
-
-        #2.4 Closing the Connection
-        #After performing database operations, always close the connection:
-        cursor.close()
-        connection.close()
-        print("Database connection closed.")
-
-
-        try:
-            connection = psycopg2.connect(
-                dbname=config['postgres']['DB_NAME'],
-                user=config['postgres']['DB_USER'],
-                password=config['postgres']['DB_PASSWORD'],
-                host=config['postgres']['DB_HOST'],
-                port=config['postgres']['DB_PORT'],
-                sslmode="require"
-            )
-            print("Database connection successful!")
-        except Exception as e:
-            print("Error connecting to the database:", e)
-        create_table(connection, "members")
-        create_table(connection, "transactions")
-        cursor = connection.cursor()
-        cursor.execute(f"""
-        INSERT INTO fin_members (first_name, last_name, email, phone_number, date_of_birth)
-        VALUES (%s, %s, %s, %s, %s);
-        """, (self.first_name, self.last_name, self.email, self.phone_number, self.date_of_birth))
-        connection.commit()
-        #id = cursor.fetchone()[0]
-        print("Record inserted successfully!")
-        #return id
+    def retrieve_member(self, email=None):
+        cursor = self.connection.cursor()
+        if email is None:
+            query = "SELECT * FROM fin_members;"
+        else:
+            query = "SELECT * FROM fin_members where email = %s ;"
+        cursor.execute(query,(email,))
+        rows = cursor.fetchall()
+        self.connection.commit()
+        if rows:
+            for row in rows:
+                print(row)
+            return rows
+        else:
+            print("No records found")
     
+    def create_new_member(self ):
+        if not self.retrieve_member(self.email):
+            cursor = self.connection.cursor()
+            cursor.execute(f"""
+            INSERT INTO fin_members (first_name, last_name, email, phone_number, date_of_birth)
+            VALUES (%s, %s, %s, %s, %s);
+            """, (self.first_name, self.last_name, self.email, self.phone_number, self.date_of_birth))
+            self.connection.commit()
+            #id = cursor.fetchone()[0]
+            print("Record inserted successfully!")
+        else:
+            print("Member already exists")
 
-new_member = Member('test@fin.com', 'Test', 'Test')
+    def update_member(self, email, first_name, last_name):
+        if self.retrieve_member(email):
+            cursor = self.connection.cursor()
+            cursor.execute("""
+            UPDATE fin_members
+            SET first_name = %s, last_name = %s
+                WHERE email = %s;
+            """, (first_name, last_name, email))
+            self.connection.commit()
+            print("Record updated successfully!")
+            print("updated data")
+            self.retrieve_member()
 
-print(new_member.member_serial_number)
-print(Member.member_serial_number)
+    def delete_member(self, email):
+        r = self.retrieve_member(email)
+        if r:
+            cursor = self.connection.cursor()
+            cursor.execute(f"""
+            DELETE FROM fin_members
+            WHERE email = %s;
+            """, (email,))
+            self.connection.commit()
+            print("Record deleted successfully!")
+        else:
+            print("Record not found")
 
-print(new_member.first_name)
+M = Member('test1@fin.com','Test1','Test1')
+print("****** Creating a new member ******")
+M.create_new_member()
+print("****** Checking member ******")
+print(M.retrieve_member('tk@fin.com'))
+print("****** Retriving all members ******")
+M.retrieve_member()
+print("****** Deleting ******")
+M.delete_member('test1@fin.com')
+print("****** Retriving all members ******")
+M.retrieve_member()
+print("****** Updating Record ******")
+M.update_member('test@fin.com', 'test_updated', 'test_updated')
 
-print(new_member.create_new_member())
+
+
